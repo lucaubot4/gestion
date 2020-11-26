0e3c720766f2240b884e226b762b067fd846b379
lucaubot4 <69604257+lucaubot4@users.noreply.github.com>
2020-11-26 20:02:49 +0100

    Update Italian.py

--- haruka/modules/translations/Italian.py
+++ haruka/modules/translations/Italian.py
@@ -1,1 +1,468 @@
 
+RUN_STRINGS  = (
+
+    "Dove pensi di andare?" ,
+
+    "Eh? Cosa? Sono scappati?" ,
+
+    "ZZzzZZzz ... Eh? Cosa? Oh, solo loro di nuovo, non importa." ,
+
+    "Torna qui!" ,
+
+    "Non così in fretta ..." ,
+
+    "Attento al muro!" ,
+
+    "Non lasciarmi solo con loro !!" ,
+
+    "Corri, muori." ,
+
+    "Scherzi su di te, sono ovunque" ,
+
+    "Te ne pentirai ..." ,
+
+    "Potresti anche provare / kickme, ho sentito che è divertente." ,
+
+    "Vai a disturbare qualcun altro, qui non interessa a nessuno." ,
+
+    "Puoi correre, ma non puoi nasconderti." ,
+
+    "È tutto quello che hai?" ,
+
+    "Sono dietro di te ..." ,
+
+    "Hai compagnia!" ,
+
+    "Possiamo farlo nel modo più semplice o nel modo più difficile". ,
+
+    "Semplicemente non capisci, vero?" ,
+
+    "Sì, faresti meglio a correre!" ,
+
+    "Per favore, ricordami quanto ci tengo?" ,
+
+    "Correrei più veloce se fossi in te." ,
+
+    "Questo è sicuramente il droide che stiamo cercando." ,
+
+    "Possano le probabilità essere sempre a tuo favore." ,
+
+    "Le ultime parole famose." ,
+
+    "E sono scomparsi per sempre, per non essere mai più visti." ,
+
+    " \" Oh, guardami! Sono così forte, posso scappare da un bot! \ " - questa persona" ,
+
+    "Sì, sì, basta toccare / calciare già." ,
+
+    "Tieni, prendi questo anello e vai da Mordor già che ci sei." ,
+
+    "Secondo la leggenda, stanno ancora correndo ..." ,
+
+    "A differenza di Harry Potter, i tuoi genitori non possono proteggerti da me." ,
+
+    "La paura porta alla rabbia. La rabbia porta all'odio. L'odio porta alla sofferenza. Se continui a correre nella paura, potresti"
+
+    "sii il prossimo Vader." ,
+
+    "Più calcoli dopo, ho deciso che il mio interesse per i tuoi imbrogli è esattamente 0." ,
+
+    "La leggenda vuole, stanno ancora correndo." ,
+
+    "Continua così, non sono sicuro che ti vogliamo qui comunque." ,
+
+    "Sei un mago- Oh. Aspetta. Non sei Harry, continua a muoverti." ,
+
+    "NIENTE CORSA NEI CORRIDOI!" ,
+
+    "Hasta la vista, piccola." ,
+
+    "Chi ha fatto uscire i cani?" ,
+
+    "È divertente, perché a nessuno importa." ,
+
+    "Ah, che spreco. Mi è piaciuto quello." ,
+
+    "Francamente, mia cara, non me ne frega niente." ,
+
+    "Il mio milkshake porta tutti i ragazzi in cortile ... Quindi corri più veloce!" ,
+
+    "Non puoi MANEGGIARE la verità!" ,
+
+    "Molto tempo fa, in una galassia molto lontana ... Qualcuno se ne sarebbe preoccupato. Non più però." ,
+
+    "Ehi, guardali! Stanno scappando dall'inevitabile banhammer ... Carino." ,
+
+    "Han ha sparato per primo. Anch'io." ,
+
+    "Cosa stai correndo dietro, un coniglio bianco?" ,
+
+    "Come direbbe il Dottore ... CORRI!" ,
+
+)
+
+INSULTS_STRINGS  = (
+
+    "Owww ... che stupido idiota." ,
+
+    "Non bere e scrivi." ,
+
+    "Penso che dovresti andare a casa o meglio un manicomio." ,
+
+    "Comando non trovato. Proprio come il tuo cervello." ,
+
+    "Ti rendi conto che ti stai prendendo in giro? Apparentemente no." ,
+
+    "Puoi scrivere meglio di così." ,
+
+    "La regola del bot 544, sezione 9, mi impedisce di rispondere a stupidi umani come te." ,
+
+    "Mi dispiace, non vendiamo cervelli." ,
+
+    "Credimi, non sei normale." ,
+
+    "Scommetto che il tuo cervello si sente come nuovo, visto che non lo usi mai." ,
+
+    "Se volessi uccidermi, scalerei il tuo ego e salterei al tuo QI." ,
+
+    "Gli zombi mangiano cervelli ... sei al sicuro." ,
+
+    "Non ti sei evoluto dalle scimmie, si sono evolute da te." ,
+
+    "Torna indietro e parlami quando il tuo QI supera la tua età." ,
+
+    "Non sto dicendo che sei stupido, sto solo dicendo che hai sfortuna quando si tratta di pensare." ,
+
+    "Che lingua parli? Perché suona come una cazzata." ,
+
+    "La stupidità non è un crimine quindi sei libero di andartene." ,
+
+    "Sei la prova che l'evoluzione PU andare al contrario." ,
+
+    "Vorrei chiederti quanti anni hai ma so che non puoi contare così in alto." ,
+
+    "Da estraneo, cosa pensi della razza umana?" ,
+
+    "I cervelli non sono tutto. Nel tuo caso non sono niente." ,
+
+    "Di solito le persone vivono e imparano. Tu vivi e basta." ,
+
+    "Non so cosa ti renda così stupido, ma funziona davvero." ,
+
+    "Continua a parlare, un giorno dirai qualcosa di intelligente! (Ne dubito però)" ,
+
+    "Scioccami, dì qualcosa di intelligente." ,
+
+    "Il tuo QI è inferiore alla misura delle tue scarpe." ,
+
+    "Ahimè! I tuoi neurotrasmettitori non funzionano più." ,
+
+    "Sei pazzo stupido." ,
+
+    "Tutti hanno il diritto di essere stupidi ma tu stai abusando del privilegio". ,
+
+    "Mi dispiace di aver ferito i tuoi sentimenti quando ti ho chiamato stupido. Pensavo che lo sapessi già." ,
+
+    "Dovresti provare ad assaggiare il cianuro." ,
+
+    "I tuoi enzimi hanno lo scopo di digerire il veleno per topi." ,
+
+    "Dovresti provare a dormire per sempre." ,
+
+    "Prendi una pistola e sparati." ,
+
+    "Potresti fare un record mondiale saltando da un aereo senza paracadute." ,
+
+    "Smettila di parlare BS e salta di fronte a un treno proiettile in corsa." ,
+
+    "Prova a fare il bagno con acido cloridrico invece che con acqua." ,
+
+    "Prova questo: se trattieni il respiro sott'acqua per un'ora, puoi trattenerlo per sempre." ,
+
+    "Diventa verde! Smetti di inalare ossigeno." ,
+
+    "Dio ti stava cercando. Dovresti andartene per incontrarlo." ,
+
+    "dai il tuo 100%. Adesso vai a donare il sangue." ,
+
+    "Prova a saltare da un edificio di cento piani, ma puoi farlo solo una volta." ,
+
+    "Dovresti donare il tuo cervello visto che non l'hai mai usato." ,
+
+    "Volontario per il bersaglio in un poligono di tiro." ,
+
+    "I colpi alla testa sono divertenti. Prendine uno." ,
+
+    "Dovresti provare a nuotare con i grandi squali bianchi." ,
+
+    "Dovresti dipingerti di rosso e correre in una maratona di tori." ,
+
+    "Puoi rimanere sott'acqua per il resto della tua vita senza tornare su." ,
+
+    "Che ne dici di smettere di respirare per tipo 1 giorno? Sarà fantastico." ,
+
+    "Prova a provocare una tigre mentre siete entrambi in gabbia." ,
+
+    "Hai provato a spararti fino a 100 m usando un cannone." ,
+
+    "Dovresti provare a tenere TNT in bocca e ad accenderlo." ,
+
+    "Prova a giocare a prendere e lanciare con RDX è divertente." ,
+
+    "Ho sentito che Phogine è velenoso, ma immagino che non ti dispiacerà inalarlo per divertimento." ,
+
+    "Lanciati nello spazio mentre dimentichi l'ossigeno sulla Terra." ,
+
+    "Dovresti provare a giocare a serpente e scale, con serpenti veri e senza scale." ,
+
+    "Balla nuda su un paio di cavi HT." ,
+
+    "True Volcano è la migliore piscina per te." ,
+
+    "Dovresti provare a fare un bagno caldo in un vulcano." ,
+
+    "Prova a passare un giorno in una bara e sarà tuo per sempre." ,
+
+    "Colpisci l'uranio con un neutrone che si muove lentamente in tua presenza. Sarà un'esperienza utile." ,
+
+    "Puoi essere la prima persona a calpestare il sole. Fai un tentativo." ,
+
+)
+
+SLAP_TEMPLATES  = (
+
+    "{user1} {hits} {user2} con un {item}." ,
+
+    "{user1} {hits} {user2} in faccia con un {item}." ,
+
+    "{user1} {hits} {user2} in giro un po 'con un {item}." ,
+
+    "{user1} {throws} a {item} at {user2}." ,
+
+    "{user1} prende un {item} e {lo lancia} in faccia a {user2}." ,
+
+    "{user1} lancia un {item} nella direzione generale di {user2}." ,
+
+    "{user1} inizia a schiaffeggiare {user2} stupidamente con un {item}." ,
+
+    "{user1} blocca {user2} e ripetutamente {hits} con un {item}." ,
+
+    "{user1} prende un {item} e {hits} {user2} con esso." ,
+
+    "{user1} lega {user2} a una sedia e {gli lancia} un {item}." ,
+
+    "{user1} ha dato una spinta amichevole per aiutare {user2} a imparare a nuotare nella lava."
+
+)
+
+ARTICOLI  = (
+
+    "padella in ghisa" ,
+
+    "trota grande" ,
+
+    "mazza da baseball" ,
+
+    "mazza da cricket" ,
+
+    "bastone di legno" ,
+
+    "chiodo" ,
+
+    "stampante" ,
+
+    "pala" ,
+
+    "Monitor CRT" ,
+
+    "libro di testo di fisica" ,
+
+    "tostapane" ,
+
+    "ritratto di Richard Stallman" ,
+
+    "televisione" ,
+
+    "camion da cinque tonnellate" ,
+
+    "rotolo di nastro adesivo" ,
+
+    "libro" ,
+
+    "laptop" ,
+
+    "vecchia televisione" ,
+
+    "sacco di pietre" ,
+
+    "trota iridea" ,
+
+    "pollo di gomma" ,
+
+    "pipistrello a spillo" ,
+
+    "estintore" ,
+
+    "rock pesante" ,
+
+    "pezzo di sporco" ,
+
+    "alveare" ,
+
+    "pezzo di carne marcia" ,
+
+    "orso" ,
+
+    "tonnellata di mattoni" ,
+
+)
+
+LANCIO  = (
+
+    "lanci" ,
+
+    "avventure" ,
+
+    "mandrini" ,
+
+    "scaglia" ,
+
+)
+
+HIT  = (
+
+    "hit" ,
+
+    "colpi" ,
+
+    "schiaffi" ,
+
+    "schiaffi" ,
+
+    "bashes" ,
+
+)
+
+MARKDOWN_HELP  =  "" "
+
+Markdown è uno strumento di formattazione molto potente supportato da Telegram. {} ha alcuni miglioramenti, per assicurarsi che \
+
+i messaggi salvati vengono analizzati correttamente e consentono di creare pulsanti.
+
+- <code> _italic_ </code>: il ritorno a capo del testo con "_" produrrà testo in corsivo
+
+- <code> * grassetto * </code>: il ritorno a capo del testo con "*" produrrà testo in grassetto
+
+- <code> `code` </code>: avvolgere il testo con '`' produrrà testo a spaziatura fissa, noto anche come 'codice'
+
+- <code> [sometext] (someURL) </code>: questo creerà un collegamento - il messaggio mostrerà solo <code> sometext </code>, \
+
+e toccandolo si aprirà la pagina in <code> someURL </code>.
+
+Ad esempio: <code> [test] (esempio.com) </code>
+
+- <code> [buttontext] (buttonurl: someURL) </code>: questo è un miglioramento speciale per consentire agli utenti di avere telegram \
+
+pulsanti nel loro markdown. <code> buttontext </code> sarà ciò che viene visualizzato sul pulsante e <code> someurl </code> \
+
+sarà l'URL che viene aperto.
+
+ES: <code> [Questo è un pulsante] (buttonurl: esempio.com) </code>
+
+Se vuoi più pulsanti sulla stessa riga, usa: stesso, come tale:
+
+<code> [one] (buttonurl: //example.com)
+
+[due] (buttonurl: //google.com: stesso) </code>
+
+Questo creerà due pulsanti su una singola riga, invece di un pulsante per riga.
+
+"" "
+
+EnglishStrings  = {
+
+    "send-start" : "" "Ciao {}, mi chiamo {}! Se hai domande su come usarmi, leggi / help e poi vai a @YanaBotGroup.
+
+Sono un bot gestore di gruppo gestito da [questa persona] (tg: // user? Id = {654839744}). Sono un fork di [Marie] (https://github.com/PaulSonOfLars/tgbot).
+
+Sono costruito in python3, usando il \
+
+libreria python-telegram-bot, e sono completamente opensource: puoi trovare ciò che mi fa battere \
+
+[qui] (https://github.com/peaktogoo/HarukaAya))!
+
+Sentiti libero di inviare richieste pull su GitHub o di contattare il mio gruppo di supporto, @EchelonTeam, per qualsiasi bug, domanda \
+
+o richieste di funzionalità che potresti avere :)
+
+Se ti piace usarmi e / o vorresti aiutarmi a sopravvivere in natura, premi / fai una donazione per finanziare / aggiornare il mio VPS!
+
+"" " ,
+
+    "send-help" : "" "Ciao! Il mio nome è * {} *.
+
+Sono un bot di gestione di gruppi modulare con alcuni divertenti extra! Dai un'occhiata a quanto segue per avere un'idea di alcune delle cose in cui posso aiutarti.
+
+Principali comandi disponibili:
+
+ - / start: ottimo comando per verificare se il bot è vivo o meno
+
+ - / help: PM sei tu questo messaggio.
+
+ - / help <nome modulo>: le informazioni del PM su quel modulo.
+
+ - / donate: informazioni su come donare!
+
+ - / lang: cambia la lingua del bot
+
+ - /impostazioni:
+
+   - in PM: ti invierà le impostazioni per tutti i moduli supportati.
+
+   - in un gruppo: ti reindirizzerà a pm, con tutte le impostazioni della chat.
+
+   {}
+
+   "" " ,
+
+    "send-group-settings" : "" "Salve! Ci sono alcune impostazioni per * {} * - vai avanti e scegli cosa
+
+che ti interessa. "" " ,
+
+#Misc
+
+"RUNS-K" : RUN_STRINGS ,
+
+"SLAP_TEMPLATES-K" : SLAP_TEMPLATES ,
+
+"ITEMS-K" : ITEMS ,
+
+"HIT-K" : HIT ,
+
+"THROW-K" : THROW ,
+
+"ITEMP-K" : ITEMS ,
+
+"ITEMR-K" : ITEMS ,
+
+"MARKDOWN_HELP-K" : MARKDOWN_HELP ,
+
+"INSULTS-K" : INSULTS_STRINGS ,
+
+#GDPR
+
+"send-gdpr" : "" "I tuoi dati personali sono stati eliminati. \ n \ n Nota che questo non verrà sbloccato \
+
+da qualsiasi chat, poiché si tratta di dati di telegramma, non di dati di YanaBot.
+
+Anche inondazioni, avvisi e gban vengono preservati, a partire da \
+
+[questo] (https://ico.org.uk/for-organisations/guide-to-the-general-data-protection-regulation-gdpr/individual-rights/right-to-erasure/), "
+
+che afferma chiaramente che il diritto alla cancellazione non si applica \
+
+\ " per l'esecuzione di un compito svolto nell'interesse pubblico \" , così com'è \
+
+il caso per i suddetti dati. "" "
+
+}
